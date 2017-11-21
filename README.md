228e01276a3598a0c0b4c7f3bd846fccd7216d55
fanich37 <icima.fan@gmail.com>
2017-11-21 14:51:13 +0300

    first commit :-)

---
+++ README.md
@@ -0,0 +1,16 @@
+# Smacc
+The simple accordion with no dependencies.
+
+## How to use it
+It's really simple:
+```
+new Smacc('.class');
+```
+or
+```
+new Smacc('#id');
+```
+Keep in mind that Smacc doesn't support complex CSS selectors (it have to be class or id), for example:
+```
+.class .class2
+```
---
+++ app/smacc.js
@@ -0,0 +1,69 @@
+;(function (root, factory) {
+	if(typeof define === 'function' && define.amd) {
+		define([], factory(root));
+	} else if(typeof exports === 'object') {
+		module.exports = factory(root);
+	} else {
+		root.Smacc = factory(root);
+	}
+})(typeof global !== 'undefined' ? global : window || this.window || this.global, function (root) {
+	'use strict';
+
+	window.Smacc = Smacc || {};
+
+	const defaultClassess = {
+		trigger: 'smacc',
+		content: 'smacc-content',
+		contentOpen: 'smacc_open'
+	};
+
+	function Smacc(settings) {
+		
+		if (typeof settings === 'string') {
+			if (settings.charAt(0) === '.') {
+				this.smacc = document.getElementsByClassName(settings.slice(1))[0];
+			} else if (settings.charAt(0) === '#') {
+				this.smacc = document.getElementById(settings.slice(1));
+			} else {
+				throw new Error('Check the selector.');
+			}
+		}
+
+		init.call(this);
+	}
+
+	function init() {
+		
+		const defaultSettings = {
+			transition: '.3s'
+		};
+		const content = this.smacc.nextElementSibling;
+
+		content.classList.add(defaultClassess.content);
+		content.style.overflow = 'hidden';
+		content.style.maxHeight = 0;
+		content.style.transition = defaultSettings.transition;
+		
+		addEvent.call(this);
+
+	}
+
+	function addEvent() {
+		
+		this.smacc.addEventListener('click', function (e) {
+			e.preventDefault();
+			e.stopPropagation();
+			const contentHeight = this.nextElementSibling.scrollHeight;
+			if (this.className.indexOf('_open') > 1) {
+				this.classList.remove(defaultClassess.contentOpen);	
+				this.nextElementSibling.style.maxHeight = 0;
+			}else {
+				this.classList.add(defaultClassess.contentOpen);
+				this.nextElementSibling.style.maxHeight = `${contentHeight}px`;
+			}
+		});
+
+	}
+
+	return Smacc;
+});
